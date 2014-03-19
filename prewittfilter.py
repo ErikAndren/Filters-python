c8b856c9e2fbca03ae2a801a0a64690bccce0945
Erik Andrén <erik.andren@gmail.com>
2014-03-19 08:06:29 +0100

    Add separate weight calculation

--- prewittfilter.py
+++ prewittfilter.py
@@ -6,10 +6,7 @@ im = Image.open("LENA512.BMP")
 
 im.show()
 
-#print(im.mode)
-
-new_image = Image.new("L", im.size, "black")
-# new_image.show()
+orig_prewitt = Image.new("L", im.size, "black")
 
 imageW = im.size[0]
 imageH = im.size[1]
@@ -30,7 +27,29 @@ for y in range(0, imageH):
 
                 res = (p1 + p2 + p3 - p7 - p8 - p9) + (p3 + p6 + p9 - p1 - p4 - p7)
                        
-                new_image.putpixel((x, y), res)
+                orig_prewitt.putpixel((x, y), res)
+orig_prewitt.show()
 
-new_image.show()
+mod_prewitt = Image.new("L", im.size, "black")
+
+for y in range(0, imageH):
+    for x in range(0, imageW):
+        if y > 0 and y < imageH-1:
+            if x > 0 and x < imageW-1:
+                p1 = im.getpixel((x-1, y-1))
+                p2 = im.getpixel((x, y-1))
+                p3 = im.getpixel((x+1, y-1))
+                p4 = im.getpixel((x-1, y))
+
+                p6 = im.getpixel((x+1, y))
+                p7 = im.getpixel((x-1, y+1))
+                p8 = im.getpixel((x, y+1))
+                p9 = im.getpixel((x+1, y+1))
+
+                res = p2 + (2*p3) - p4 + p6 - (2*p7) - p8
+                       
+                mod_prewitt.putpixel((x, y), res)
+
+mod_prewitt.show()
+
 
